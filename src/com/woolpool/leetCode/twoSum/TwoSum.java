97d657b8ec2dea7c49d92cdd8d37dabf047e0b98
damonMPR <damon.cwz.xmu@gmail.com>
2015-08-24 21:35:34 +0800

    Two Sum

---
+++ src/com/woolpool/leetCode/twoSum/TwoSum.java
@@ -0,0 +1,34 @@
+package com.woolpool.leetCode.twoSum;
+
+/**
+ * 模块: [该类所处的模块功能说明]&lt;br&gt;
+ * 用途: [该类实现的功能说明]&lt;br&gt;
+ * 版权: Copyright (c) 2012-${year} woolpool,Corp.&lt;br&gt;
+ * Created by IntelliJ IDEA.
+ * User: woolpool
+ * Date: 15/8/24
+ * Time: 下午9:24
+ * Email: wzchenxm@163.com
+ * 版本号:日期:作者:提交标识:说明<br>
+ * -------------------------------------------------------------------------------------<br>
+ * 001:2015/7/6:woolpool:gitLog:新建<br>
+ * <br>
+ */
+
+/**
+ * Given an array of integers, find two numbers such that they add up to a specific target number.
+
+ * The function twoSum should return indices of the two numbers such that they add up to the target, where index1 must be less than index2. Please note that your returned answers (both index1 and index2) are not zero-based.
+
+ * You may assume that each input would have exactly one solution.
+
+ * Input: numbers={2, 7, 11, 15}, target=9
+ * Output: index1=1, index2=2
+ */
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        int[] a = {1, 1};
+
+        return  a;
+    }
+}
