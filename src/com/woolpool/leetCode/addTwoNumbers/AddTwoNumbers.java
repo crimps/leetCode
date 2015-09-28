b7772548e65dda78ab162118de91be51b81babbf
damonMPR <damon.cwz.xmu@gmail.com>
2015-09-28 17:42:44 +0800

    add Two numbers

---
+++ src/com/woolpool/leetCode/addTwoNumbers/AddTwoNumbers.java
@@ -0,0 +1,40 @@
+package com.woolpool.leetCode.addTwoNumbers;
+
+/**
+ * 模块: [该类所处的模块功能说明]&lt;br&gt;
+ * 用途: [该类实现的功能说明]&lt;br&gt;
+ * 版权: Copyright (c) 2012-2015 woolpool,Corp.&lt;br&gt;
+ * Created by IntelliJ IDEA.
+ * User: woolpool
+ * Date: 15/9/28
+ * Time: 下午5:24
+ * Email: wzchenxm@163.com
+ * 版本号:日期:作者:提交标识:说明<br>
+ * -------------------------------------------------------------------------------------<br>
+ * 001:15/9/28:woolpool:gitLog:新建<br>
+ * <br>
+ */
+
+/**
+ *
+ * You are given two linked lists representing two non-negative numbers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+
+ * Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+ * Output: 7 -> 0 -> 8
+ */
+public class AddTwoNumbers {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode listNode = new ListNode(1);
+
+        return listNode;
+    }
+}
+
+/**
+ * Definition for singly-linked list.
+ */
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
