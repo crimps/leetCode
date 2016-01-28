f2d7e7f9952ba5bb842f16374f31894174da9e8e
damon <damon.cwz.xmu@gmail.com>
2016-01-28 15:58:42 +0800

    LongestSubstringWithoutRepeatingCharacters

---
+++ src/com/woolpool/leetCode/LongestSubstringWithoutRepeatingCharacters/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,12 @@
+package com.woolpool.leetCode.LongestSubstringWithoutRepeatingCharacters;
+
+/**
+ *
+ */
+public class LongestSubstringWithoutRepeatingCharacters {
+    public int lengthOfLongestSubstring(String s) {
+        int length = 0;
+
+        return length;
+    }
+}
---
+++ test/com/woolpool/leetCode/LongestSubstringWithoutRepeatingCharacters/LongestSubstringWithoutRepeatingCharactersTest.java
@@ -0,0 +1,15 @@
+package com.woolpool.leetCode.LongestSubstringWithoutRepeatingCharacters;
+
+import org.junit.Test;
+
+/**
+ *
+ */
+public class LongestSubstringWithoutRepeatingCharactersTest {
+    private String str1 = "abcabcbb";
+    private String str2 = "bbbbb";
+
+    @Test public void testLengthOfLongestSubstring() throws Exception {
+        
+    }
+}
