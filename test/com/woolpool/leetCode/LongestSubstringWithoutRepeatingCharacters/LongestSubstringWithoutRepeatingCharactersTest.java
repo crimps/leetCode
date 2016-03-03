30cb06db3bfbda920f8ca389fadffda16289df68
damon <damon.cwz.xmu@gmail.com>
2016-03-03 14:26:20 +0800

    LongestSubstringWithoutRepeatingCharacters

--- test/com/woolpool/leetCode/LongestSubstringWithoutRepeatingCharacters/LongestSubstringWithoutRepeatingCharactersTest.java
+++ test/com/woolpool/leetCode/LongestSubstringWithoutRepeatingCharacters/LongestSubstringWithoutRepeatingCharactersTest.java
@@ -6,11 +6,14 @@ import org.junit.Test;
  *
  */
 public class LongestSubstringWithoutRepeatingCharactersTest {
-    private String str1 = "abcabcbb";
+    private String str1 = "aabcdabcdad";
     private String str2 = "bbbbb";
 
     @Test public void testLengthOfLongestSubstring() throws Exception {
         LongestSubstringWithoutRepeatingCharacters longestSubstringWithoutRepeatingCharacters = new LongestSubstringWithoutRepeatingCharacters();
+        long start = System.nanoTime();
         longestSubstringWithoutRepeatingCharacters.lengthOfLongestSubstring(str1);
+        long end = System.nanoTime();
+        System.out.println("total time : " + (end - start) + "ns");
     }
 }
