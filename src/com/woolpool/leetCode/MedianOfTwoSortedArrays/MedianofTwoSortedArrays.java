b13a4de4bd1d017997405777936fa52edc18d6ce
damonMPR <woolpack.cwz@gmail.com>
2016-05-20 16:58:05 +0800

    MedianOfTwoSortedArrays

--- src/com/woolpool/leetCode/MedianOfTwoSortedArrays/MedianofTwoSortedArrays.java
+++ src/com/woolpool/leetCode/MedianOfTwoSortedArrays/MedianofTwoSortedArrays.java
@@ -1,5 +1,8 @@
 package com.woolpool.leetCode.MedianOfTwoSortedArrays;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * There are two sorted arrays nums1 and nums2 of size m and n respectively.
  * Find the median of the two sorted arrays.
@@ -7,13 +10,34 @@ package com.woolpool.leetCode.MedianOfTwoSortedArrays;
  */
 public class MedianOfTwoSortedArrays {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        double result = 0;
-
-        double medianLength = (nums1.length + nums2.length)/2;
-        for(int i = 0, j = 0; i < nums1.length && j < nums2.length;) {
-
+        List<Integer> nums = new ArrayList<>();
+        int index1 = 0;
+        int index2 = 0;
+        for (; index1 < nums1.length && index2 < nums2.length; ) {
+            if (nums1[index1] > nums2[index2]) {
+                nums.add(nums2[index2]);
+                index2 ++;
+            } else {
+                nums.add(nums1[index1]);
+                index1 ++;
+            }
         }
-
-        return  result;
+        if (index1 < nums1.length) {
+            for (; index1 < nums1.length; ) {
+                nums.add(nums1[index1]);
+                index1 ++;
+            }
+        }
+        if (index2 < nums2.length) {
+            for (; index2 < nums2.length; ) {
+                nums.add(nums2[index2]);
+                index2 ++;
+            }
+        }
+        if (nums.size() % 2 == 0) {
+            return (double)(nums.get(nums.size() / 2 - 1) + nums.get(nums.size() / 2)) / 2;
+        } else {
+            return (double)nums.get((nums.size() + 1) / 2 - 1);
+        }
     }
 }
--- test/com/woolpool/leetCode/MedianOfTwoSortedArrays/MedianofTwoSortedArraysTest.java
+++ test/com/woolpool/leetCode/MedianOfTwoSortedArrays/MedianofTwoSortedArraysTest.java
@@ -5,11 +5,24 @@ import org.junit.Test;
 public class MedianOfTwoSortedArraysTest {
     private int[] type1_number1 = {1,2,3,4,5};
     private int[] type1_number2 = {2,3,4,5,6};
+
+    private int[] type2_number1 = {};
+    private int[] type2_number2 = {1};
+
+    private int[] type3_number1 = {};
+    private int[] type3_number2 = {2, 3};
+
     private MedianOfTwoSortedArrays medianofTwoSortedArrays = new MedianOfTwoSortedArrays();
 
     @Test
     public void testFindMedianSortedArrays() throws Exception {
         double result = medianofTwoSortedArrays.findMedianSortedArrays(type1_number1, type1_number2);
         System.out.println(result);
+
+        double result2 = medianofTwoSortedArrays.findMedianSortedArrays(type2_number1, type2_number2);
+        System.out.println(result2);
+
+        double result3 = medianofTwoSortedArrays.findMedianSortedArrays(type2_number1, type3_number2);
+        System.out.println(result3);
     }
 }
