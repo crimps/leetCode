b8860f3e043779ae7c86f6e3fab3f82e063c6163
homePc <woolpack.cwz@gmail.com>
2016-05-21 12:02:29 +0800

    Longest Palindromic Substring

---
+++ src/com/woolpool/leetCode/LongestPalindromicSubstring/LongestPalindromicSubstring.java
@@ -0,0 +1,10 @@
+package com.woolpool.leetCode.LongestPalindromicSubstring;
+
+/**
+ * Created by Administrator on 2016/5/21.
+ */
+public class LongestPalindromicSubstring {
+    public String longestPalindrome(String s) {
+        return null;
+    }
+}
