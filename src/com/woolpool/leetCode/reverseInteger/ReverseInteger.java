8dee96b2d79dd492b270b9c97552f794b9162a41
damonMPR <woolpack.cwz@gmail.com>
2016-11-28 19:22:44 +0800

    reverse integer

---
+++ src/com/woolpool/leetCode/reverseInteger/ReverseInteger.java
@@ -0,0 +1,23 @@
+/**
+ * @(#)com.woolpool.leetCode.reverseInteger.ReverseInteger.java Copyright (c) 2014-2018 crimps
+ */
+package com.woolpool.leetCode.reverseInteger;
+
+/**
+ *
+ * @author crimps
+ * @version 1.0  16/11/28
+ * @modified crimps  16/11/28  <创建>
+ */
+
+/**
+ * Reverse digits of an integer.
+
+ Example1: x = 123, return 321
+ Example2: x = -123, return -321
+ */
+public class ReverseInteger {
+    public int reverse(int x) {
+        return 0;
+    }
+}
