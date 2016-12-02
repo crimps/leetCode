1f4d2b8f15ace8d656d052887690a15b4d7b603c
damonMPR <woolpack.cwz@gmail.com>
2016-12-02 16:23:00 +0800

    Regular Expression

---
+++ src/com/woolpool/leetCode/regularExpressionMatching/RegularExpressionMatching.java
@@ -0,0 +1,37 @@
+/**
+ * @(#)com.woolpool.leetCode.regularExpressionMatching.RegularExpressionMatching.java Copyright (c) 2014-2018 crimps
+ */
+package com.woolpool.leetCode.regularExpressionMatching;
+
+/**
+ *
+ * @author crimps
+ * @version 1.0  16/12/2
+ * @modified crimps  16/12/2  <创建>
+ */
+
+/**
+ * Implement regular expression matching with support for '.' and '*'.
+
+ '.' Matches any single character.
+ '*' Matches zero or more of the preceding element.
+
+ The matching should cover the entire input string (not partial).
+
+ The function prototype should be:
+ bool isMatch(const char *s, const char *p)
+
+ Some examples:
+ isMatch("aa","a") → false
+ isMatch("aa","aa") → true
+ isMatch("aaa","aa") → false
+ isMatch("aa", "a*") → true
+ isMatch("aa", ".*") → true
+ isMatch("ab", ".*") → true
+ isMatch("aab", "c*a*b") → true
+ */
+public class RegularExpressionMatching {
+    public boolean isMatch(String s, String p) {
+        return true;
+    }
+}
---
+++ test/com/woolpool/leetCode/regularExpressionMatching/RegularExpressionMatchingTest.java
@@ -0,0 +1,18 @@
+package com.woolpool.leetCode.regularExpressionMatching;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author crimps
+ * @version 1.0  16/12/2
+ * @modified crimps  16/12/2  <创建>
+ */
+public class RegularExpressionMatchingTest {
+    @Test
+    public void isMatch() throws Exception {
+
+    }
+
+}
