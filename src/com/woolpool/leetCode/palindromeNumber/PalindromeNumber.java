b0ac0aeb0b173f63818480b04960ed17bb1b5866
damonMPR <woolpack.cwz@gmail.com>
2016-12-02 14:15:38 +0800

    Palindrome Number

---
+++ src/com/woolpool/leetCode/palindromeNumber/PalindromeNumber.java
@@ -0,0 +1,29 @@
+/**
+ * @(#)com.woolpool.leetCode.palindromeNumber.PalindromeNumber.java Copyright (c) 2014-2018 crimps
+ */
+package com.woolpool.leetCode.palindromeNumber;
+
+/**
+ *
+ * @author crimps
+ * @version 1.0  16/12/2
+ * @modified crimps  16/12/2  <创建>
+ */
+
+/**
+ * Determine whether an integer is a palindrome. Do this without extra space.
+
+ Some hints:
+ Could negative integers be palindromes? (ie, -1)
+
+ If you are thinking of converting the integer to string, note the restriction of using extra space.
+
+ You could also try reversing an integer. However, if you have solved the problem "Reverse Integer", you know that the reversed integer might overflow. How would you handle such case?
+
+ There is a more generic way of solving this problem.
+ */
+public class PalindromeNumber {
+    public boolean isPalindrome(int x) {
+        return true;
+    }
+}
---
+++ test/com/woolpool/leetCode/palindromeNumber/PalindromeNumberTest.java
@@ -0,0 +1,18 @@
+package com.woolpool.leetCode.palindromeNumber;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author crimps
+ * @version 1.0  16/12/2
+ * @modified crimps  16/12/2  <创建>
+ */
+public class PalindromeNumberTest {
+    @Test
+    public void isPalindrome() throws Exception {
+
+    }
+
+}
