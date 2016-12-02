54734d3c09b547c295913077d2f6f48d4448c8d5
damonMPR <woolpack.cwz@gmail.com>
2016-12-02 15:11:06 +0800

    Palindrome Number

--- src/com/woolpool/leetCode/palindromeNumber/PalindromeNumber.java
+++ src/com/woolpool/leetCode/palindromeNumber/PalindromeNumber.java
@@ -23,7 +23,45 @@ package com.woolpool.leetCode.palindromeNumber;
  There is a more generic way of solving this problem.
  */
 public class PalindromeNumber {
+    /**
+     * 转化为字符串，从首尾各取字符依次比较，直到结束都一样则为回文，否则不为回文
+     * 时间复杂度：O(n)
+     * @param x
+     * @return
+     */
     public boolean isPalindrome(int x) {
+        //负数都不是回文
+        if (x < 0) {
+            return false;
+        }
+        //单位数字都是回文
+        if (0 <= x && x <= 9) {
+            return true;
+        } else {
+            String integerStr = String.valueOf(x);
+            for(int start_index = 0, end_index = integerStr.length() - 1; start_index < integerStr.length();
+                start_index++, end_index--) {
+                if (integerStr.charAt(start_index) != integerStr.charAt(end_index)) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
+    public boolean isPalindrome_type2(int x) {
+        //负数都不是回文
+        if (x < 0) {
+            return false;
+        }
+        //单位数字都是回文
+        if (0 <= x && x <= 9) {
+            return true;
+        } else {
+            for(int i = 0; i < Integer.valueOf(x).toString().length(); i++) {
+
+            }
+        }
         return true;
     }
 }
--- test/com/woolpool/leetCode/palindromeNumber/PalindromeNumberTest.java
+++ test/com/woolpool/leetCode/palindromeNumber/PalindromeNumberTest.java
@@ -10,9 +10,13 @@ import static org.junit.Assert.*;
  * @modified crimps  16/12/2  <创建>
  */
 public class PalindromeNumberTest {
+    private String EQUALS = "equals";
+    PalindromeNumber palindromeNumber = new PalindromeNumber();
     @Test
     public void isPalindrome() throws Exception {
-
+        assertEquals(EQUALS, false, palindromeNumber.isPalindrome(10));
+        assertEquals(EQUALS, false, palindromeNumber.isPalindrome(-2147483648));
+        assertEquals(EQUALS, true, palindromeNumber.isPalindrome(121));
     }
 
 }
